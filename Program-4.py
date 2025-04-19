c76ac3aeb4c64cefef2c5d49212d5d59ccb62a9e
Dommaraju Sasi Kumar Raju <123371232+dskr1234@users.noreply.github.com>
2025-04-20 00:39:28 +0530

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -4,7 +4,7 @@
 def countmultiple(values_list):
     dict_vaules = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
     for i in range(values_list):
-        for j in (9):
+        for j in range(9):
             if i % (j+1) == 0:
                 dict_vaules[j+1]+=1
     return dict_vaules
