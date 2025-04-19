a3989336b2043d55dfb4e0400277eba2efaaf07f
Dommaraju Sasi Kumar Raju <123371232+dskr1234@users.noreply.github.com>
2025-04-20 00:41:03 +0530

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -4,8 +4,8 @@
 def countmultiple(values_list):
     dict_vaules = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
     for i in values_list:
-        for j in range(9):
-            if i % (j+1) == 0:
+        for j in range(1,10):
+            if i % j == 0:
                 dict_vaules[j+1]+=1
     return dict_vaules
 values_list = [int(i) for i in input('Enter intergers with spaces like 1 2 3 ... ').split()]
